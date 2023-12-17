9bf939421fee03a3a988e5b9232d45bf3f34c247
Naumenko Tatiana <jouk@list.ru>
2023-12-17 11:23:49 +0300

    Create an environment and add first file App.java

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    application
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
