c589ae787c05bec9a0a7732122b7f0f16b590183
Naumenko Tatiana <jouk@list.ru>
2023-12-17 17:51:39 +0300

    Modify file App.java and create file Cli.java to great and get user name

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain games!");
+        System.out.println("May I have your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
+}
