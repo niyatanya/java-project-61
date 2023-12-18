d3d051912de88ceea35355f65803f7137cadb03f
Naumenko Tatiana <jouk@list.ru>
2023-12-18 08:58:17 +0300

    Add checkstyle plugin to build.gradle.kts  and rules to repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     application
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.50.0"
+    checkstyle
 }
 
 group = "hexlet.code"
