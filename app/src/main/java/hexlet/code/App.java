d34a77290d1d6f8ed06e55fc30785a6b72f8824f
Naumenko Tatiana <jouk@list.ru>
2023-12-21 20:58:48 +0300

    Add file Even.java and modify App.java accordingly

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,6 +5,7 @@ class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
 
         Scanner scanner1 = new Scanner(System.in);
@@ -14,6 +15,9 @@ class App {
         switch (userChoice) {
             case 1:
                 Cli.greeting();
+                break;
+            case 2:
+                Even.playEven();
                 break;
             case 0:
                 break;
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,45 @@
+package hexlet.code;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Even {
+    public static void playEven() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        int correctAnswersCount = 0;
+        while (correctAnswersCount < 3) {
+
+            Random random = new Random();
+            int num1 = random.nextInt(100);
+            System.out.println("Question: " + num1);
+
+            String correctAnswer1;
+            if (num1 % 2 == 0) {
+                correctAnswer1 = "yes";
+            } else {
+                correctAnswer1 = "no";
+            }
+
+            String userAnswer1 = scanner.nextLine();
+
+            if (userAnswer1.equals(correctAnswer1)) {
+                System.out.println("Correct!");
+                correctAnswersCount++;
+            } else {
+                System.out.println("'" + userAnswer1 + "' is wrong answer ;(. Correct answer was '" + correctAnswer1 + "'.");
+                System.out.println("Let's try again, " + name + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + name + "!");
+        scanner.close();
+    }
+}
