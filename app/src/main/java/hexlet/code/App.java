778ba176d00743b6ffb391cb38e28aa5440d7586
Naumenko Tatiana <jouk@list.ru>
2023-12-26 09:59:15 +0300

    Modify App.java and GCD.java to add new game GCD

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,6 +7,7 @@ class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
 
         Scanner scanner1 = new Scanner(System.in);
@@ -22,6 +23,10 @@ class App {
                 break;
             case 3:
                 Calc.playCalc();
+                break;
+            case 4:
+                GCD.playGCD();
+                break;
             case 0:
                 break;
             default:
---
+++ app/src/main/java/hexlet/code/GCD.java
@@ -0,0 +1,46 @@
+package hexlet.code;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class GCD {
+    public static void playGCD() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Find the greatest common divisor of given numbers");
+
+        int correctAnswersCount = 0;
+        while (correctAnswersCount < 3) {
+
+            Random random = new Random();
+            int num1 = random.nextInt(100);
+            int num2 = random.nextInt(100);
+            System.out.println("Question: " + num1 + " " + num2);
+
+            int correctAnswer = 0;
+            for (int i = 1; i <= num1 && i <= num2; i++) {
+                if (num1 % i == 0 && num2 % i == 0) {
+                    correctAnswer = i;
+                }
+            }
+
+            int userAnswer = scanner.nextInt();
+
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+                correctAnswersCount++;
+            } else {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                System.out.println("Let's try again, " + name + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + name + "!");
+        scanner.close();
+    }
+}
