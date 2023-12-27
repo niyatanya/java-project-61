b4a25e84b8c8db01a7c46012d3a616159b9d11a2
Naumenko Tatiana <jouk@list.ru>
2023-12-27 19:50:16 +0300

    Correct typo in Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting() {
         Scanner scanner2 = new Scanner(System.in);
-        System.out.println("Welcome to the Brain games!");
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
 
         String name = scanner2.nextLine();
