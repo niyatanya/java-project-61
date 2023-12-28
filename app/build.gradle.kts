ee0b350825d850f9fcf88749e2f3c05a1b1b7e1b
Naumenko Tatiana <jouk@list.ru>
2023-12-28 22:56:06 +0300

    Modify build.gradle.kts to delete unusable test dependencies and Java plagin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     application
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.50.0"
@@ -13,19 +12,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
 application {
     mainClass.set("hexlet.code.App")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
 }
